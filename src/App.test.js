689492d35470f6071b7fd5700d5b9b0264554e1e
stefan <stefan@stefans-iMac.local>
2020-12-10 18:56:17 +0100

    Typing into todo input

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,10 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import App from "./App";
 
 test("Add todo item", () => {
   render(<App />);
   const todoInput = screen.getByPlaceholderText(/enter todo item/i);
+  userEvent.type(todoInput, "Wash the dishes{enter}");
+  const todoItem = screen.getByText("Wash the dishes");
 });
