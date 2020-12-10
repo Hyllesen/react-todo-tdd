3ac7fa245aa856187d51aea35f335835f3637aee
stefan <stefan@stefans-iMac.local>
2020-12-10 20:06:07 +0100

    Added test with add todo button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { nanoid } from "nanoid";
 
 function App() {
   const [todos, setTodos] = useState([]);
@@ -8,13 +9,13 @@ function App() {
         type="text"
         onKeyDown={(e) => {
           if (e.key === "Enter") {
-            setTodos([...todos, { title: e.target.value }]);
+            setTodos([...todos, { title: e.target.value, id: nanoid() }]);
           }
         }}
         placeholder="Enter todo item"
       />
       {todos.map((todo) => (
-        <div>{todo.title}</div>
+        <div key={todo.id}>{todo.title}</div>
       ))}
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -2,9 +2,20 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import App from "./App";
 
-test("Add todo item", () => {
-  render(<App />);
-  const todoInput = screen.getByPlaceholderText(/enter todo item/i);
-  userEvent.type(todoInput, "Wash the dishes{enter}");
-  const todoItem = screen.getByText("Wash the dishes");
+describe("Todo App", () => {
+  beforeEach(() => {
+    render(<App />);
+  });
+
+  test("Add todo item by enter key", () => {
+    const todoInput = screen.getByPlaceholderText(/enter todo item/i);
+    userEvent.type(todoInput, "Wash the dishes{enter}");
+    const todoItem = screen.getByText("Wash the dishes");
+  });
+  test("Add todo item by add todo button", () => {
+    const todoInput = screen.getByPlaceholderText(/enter todo item/i);
+    userEvent.type(todoInput, "Do the laundry");
+    const addTodoButton = screen.getByText(/add todo/i);
+    const todoItem = screen.getByText("Do the laundry");
+  });
 });
