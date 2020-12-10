f0752eead307d279ac0876373f02a1c5652b54b1
stefan <stefan@stefans-iMac.local>
2020-12-10 19:40:04 +0100

    Test passing for adding todo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { useState } from "react";
+
 function App() {
+  const [todos, setTodos] = useState([]);
   return (
     <div className="App">
-      <input type="text" placeholder="Enter todo item" />
+      <input
+        type="text"
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            setTodos([...todos, { title: e.target.value }]);
+          }
+        }}
+        placeholder="Enter todo item"
+      />
+      {todos.map((todo) => (
+        <div>{todo.title}</div>
+      ))}
     </div>
   );
 }
